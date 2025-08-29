7cfa53f761c48e6424889f31d753b7f06d326456
unknown <lab3121@etecjk.local>
2025-08-29 14:08:35 -0300

    Commit inicial

---
+++ conexao.php
@@ -0,0 +1,15 @@
+<?php 
+$host = 'localhost';
+$port="3306";
+$user="root";
+$senha="";
+$banco="Pessoa";
+
+try{
+    $conn=new PDO('mysql:host='.$host.';port='.$port.';dbname='.$banco,$user,$senha);
+    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+    echo 'ERROR'.$e->getMessege();
+}
+
+?>
