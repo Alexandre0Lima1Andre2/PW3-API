006bb5f8ef9464ed774d751b59b1e3dadc64dbe1
EmersonBragaETEC <emerson.braga@etec.sp.gov.br>
2025-08-29 14:35:43 -0300

    Update Pessoa.sql

--- Pessoa.sql
+++ Pessoa.sql
@@ -4,8 +4,8 @@ use Pessoa;
 create table Pessoa(
 id_pessoa int primary key auto_increment not null,
 nome_pessoa varchar(200),
-cpf_pessoa char(11),
-telefone_pessoa char(15)
+cpf_pessoa varchar(11) unique,
+telefone_pessoa varchar(15)
 );
 
 drop table Pessoa;
