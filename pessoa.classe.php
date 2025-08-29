6f270b059a2c34975c9b93702ab2ac0f16492014
unknown <lab3121@etecjk.local>
2025-08-29 14:57:47 -0300

    nova alteração

--- pessoa.classe.php
+++ pessoa.classe.php
@@ -1,4 +1,5 @@
 <?php
+    
     class Pessoa{
         public $id_pessoa;
         public $nome_pessoa;
@@ -6,15 +7,18 @@
         public $telefone_pessoa;
 
         public function crate(){
-            
+            $sql = "insert into";
         }
         public function read(){
-
+            $sql = "select * from ".this->table;
+            
         }
         public function update(){
-
+            $sql = "update ";
+            
         }
         public function delet(){
+            $sql = "delet ";
 
         }
     }
